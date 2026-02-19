0b77883ce1c878906a15562438aef4765a606675
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-19 11:39:15 -0300

    ex01_NomeIdade

---
+++ src/Semana1Introducao/Semana1Exercicios/Ex01_NomeIdade.java
@@ -0,0 +1,10 @@
+package Semana1Introducao.Semana1Exercicios;
+
+public class Ex01_NomeIdade {
+    public static void main (String[] args){
+        String nome = "Bruno";
+        int idade = 18;
+
+        System.out.println("Meu nome é "+nome+", e tenho "+idade+" anos");
+    }
+}
