00966e7d428c5b68a43293379ee3a4a57a390acb
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-21 23:41:17 -0300

    exercicios

--- src/JavaBasicoExercicios/Ex02_Imposto_If.java
+++ src/JavaBasicoExercicios/Ex02_Imposto_If.java
@@ -5,14 +5,14 @@ public class Ex02_Imposto_If {
         //se salary <= 34712 paga 9.7% de imposto
         // se salary >= 34713 && salary <= 68507 paga 37.55% de imposto
         // se salary >= 68508 paga  49.5%
-        double annualSalary = 40000;
+        double annualSalary = 70000;
         double taxRate;
-        if(salary <= 34712){
-            taxRate = salary * (9.7/100);
-        }else if(salary >= 34713 && salary <= 68507){
-            taxRate = salary * (37.55/100);
+        if(annualSalary <= 34712){
+            taxRate = annualSalary * (9.7/100);
+        }else if(annualSalary >= 34713 && annualSalary <= 68507){
+            taxRate = annualSalary * (37.55/100);
         }else{
-            taxRate = salary * (49.5/100);
+            taxRate = annualSalary * (49.5/100);
         }
         System.out.println("O total de imposto a ser pago é: "+taxRate);
     }
