a0ee8695180a2a4f51dfdd639344bb4d7e3b89a8
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-22 00:10:26 -0300

    switch

---
+++ src/JavaBasico/Aula04EstruturasCondicionais/Aula05EstruturasCondicionais04.java
@@ -0,0 +1,47 @@
+package JavaBasico.Aula04EstruturasCondicionais;
+
+public class Aula05EstruturasCondicionais04 {
+    public static void main(String[] args) {
+        // imprima o dia da semana, considere 1 como domingo
+        //switch : char,int,byte,short,enum,String
+        byte day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("dom");
+                break;
+            case 2:
+                System.out.println("seg");
+                break;
+            case 3:
+                System.out.println("ter");
+                break;
+            case 4:
+                System.out.println("quar");
+                break;
+            case 5:
+                System.out.println("quin");
+            case 6:
+                System.out.println("sex");
+                break;
+            case 7:
+                System.out.println("sab");
+                break;
+            default:
+                System.out.println("Opçoes invalidas");
+                break;
+        }
+        //indentificar o sexo feminimo e masculino
+        char gender = 'M';
+        switch (gender){
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Femenino");
+                break;
+            default:
+                System.out.println("Opcao invalida");
+                break;
+        }
+    }
+}
