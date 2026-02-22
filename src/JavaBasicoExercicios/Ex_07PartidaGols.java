e1c2c7260eb29be9e4ab03d72a3515459c961a0d
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-22 14:58:30 -0300

    exercicios

---
+++ src/JavaBasicoExercicios/Ex_07PartidaGols.java
@@ -0,0 +1,25 @@
+package JavaBasicoExercicios;
+
+public class Ex_07PartidaGols {
+    public static void main(String[] args) {
+        int time1 = 2;
+        int time2 = 7;
+        int difference;
+
+        if (time1 > time2) {
+            difference = time1 - time2;
+        } else {
+            difference = time2 - time1;
+        }
+
+        System.out.println("A diferenca de gols foi de "+difference);
+
+        if (difference >= 4) {
+            System.out.println("Goleada");
+        } else if (difference <= 3 && difference >= 1) {
+            System.out.println("Partida normal");
+        } else {
+            System.out.println("Empate");
+        }
+    }
+}
