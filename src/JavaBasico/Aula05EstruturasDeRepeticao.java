040a7ed42d027b54f3f84b011976ea2162783fde
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-22 15:50:26 -0300

    estrturacondicionais01

---
+++ src/JavaBasico/Aula05EstruturasDeRepeticao.java
@@ -0,0 +1,17 @@
+package JavaBasico;
+
+public class Aula05EstruturasDeRepeticao {
+    public static void main(String[] args) {
+        //while,do while,for
+        int count = 0;
+        while(count < 10){
+            count++;
+            System.out.println("Contador while:"+count);
+        }
+        count = 0;
+        do{
+            count++;
+            System.out.println("Contador do-while: "+count);
+        }while(count < 10);
+    }
+}
