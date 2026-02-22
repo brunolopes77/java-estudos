689dbcea164d7bc755476cb235d98241fd758e1a
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-22 12:24:15 -0300

    exercicios

---
+++ src/JavaBasicoExercicios/Ex05_Aprovacao.java
@@ -0,0 +1,19 @@
+package JavaBasicoExercicios;
+
+public class Ex05_Aprovacao {
+    public static void main(String[] args) {
+        //nota1 e nota 2, se a media > 7 aluno aprovado,se nao reprovado
+        double grade1 = 34;
+        double grade2 = 23;
+        double average;
+        String result1 = "aprovado";
+        String result2 = " reprovado";
+
+        average = (grade1 + grade2)/2;
+        if(average < 7){
+            System.out.println("A media foi de "+average+", e o aluno está "+result2);
+        }else{
+            System.out.println("A media foi de "+average+", e o aluno está "+result1);
+        }
+    }
+}
---
+++ src/JavaBasicoExercicios/Ex06_AproveitamentoDeUmAluno.java
@@ -0,0 +1,27 @@
+package JavaBasicoExercicios;
+
+public class Ex06_AproveitamentoDeUmAluno {
+    public static void main(String[] args) {
+     // A proveitamendo de um aluno:A,B,C,D,E,F
+        double grade1 =7;
+        double grade2 =4;
+        double average;
+
+        average = (grade1+grade2)/2;
+        if(average > 10 || average < 0) {
+            System.out.println("Nota colocada errada");
+        } else if (average >= 9) {
+            System.out.println("A media foi de "+average+" e o aproveitamento foi de: A");
+        }else if (average >= 8) {
+            System.out.println("A media foi de "+average+" e o aproveitamento foi de: B");
+        }else if (average >= 7) {
+            System.out.println("A media foi de "+average+" e o aproveitamento foi de: C");
+        }else if (average >= 6) {
+            System.out.println("A media foi de "+average+" e o aproveitamento foi de: D");
+        }else if (average >= 5) {
+            System.out.println("A media foi de "+average+" e o aproveitamento foi de: E");
+        }else {
+            System.out.println("A media foi de " + average + " e o aproveitamento foi de: F");
+        }
+    }
+}
