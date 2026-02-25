e777f89b9ecaad28d1ba61d10ad1ef708275d305
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-25 12:30:55 -0300

    organization

--- src/JavaBasico/Aula06Arrays/ArraysMultidimensionais02.java
+++ src/JavaBasico/Aula06Arrays/ArraysMultidimensionais02.java
@@ -1,7 +1,7 @@
 package JavaBasico.Aula06Arrays;
 
-public class ArraysMultidimensionais03 {
+public class ArraysMultidimensionais02 {
     public static void main(String[] args) {
-        
+
     }
 }
