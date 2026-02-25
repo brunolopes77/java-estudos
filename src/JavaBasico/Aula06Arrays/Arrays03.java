9059809519d8d3b228350749cbf8458cb21894d6
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-25 12:07:41 -0300

    arrays

---
+++ src/JavaBasico/Aula06Arrays/Arrays03.java
@@ -0,0 +1,14 @@
+package JavaBasico.Aula06Arrays;
+
+public class Arrays03 {
+    public static void main(String[] args) {
+        int [] numeros1 = {1,2,3,4,5,};
+        int [] numeros2 = new int[]{5,4,3,2,1};
+        for (int i = 0; i < numeros1.length; i++) {
+            System.out.println(numeros2[i]);
+        }
+        for (int num : numeros2){
+            System.out.println(num);
+        }
+    }
+}
---
+++ src/JavaBasico/Aula06Arrays/ArraysMultidimensionais03.java
@@ -0,0 +1,7 @@
+package JavaBasico.Aula06Arrays;
+
+public class ArraysMultidimensionais03 {
+    public static void main(String[] args) {
+        
+    }
+}
