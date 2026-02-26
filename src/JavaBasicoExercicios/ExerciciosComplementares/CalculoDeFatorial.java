a8c7526b8e6e68d20b032076ae3b644444cdf203
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-26 19:08:56 -0300

    exercicios

--- src/JavaBasicoExercicios/ExerciciosComplementares/CalculoDeFatorial.java
+++ src/JavaBasicoExercicios/ExerciciosComplementares/CalculoDeFatorial.java
@@ -2,20 +2,11 @@ package JavaBasicoExercicios.ExerciciosComplementares;
 
 public class CalculoDeFatorial {
     public static void main(String[] args) {
-        int numeroFatorial = 1;
-        int resultMultiplicacao = numeroFatorial * (numeroFatorial - 1);
-
-        if (numeroFatorial <= 3 && numeroFatorial >= 2) {
-            System.out.println(resultMultiplicacao);
-        } else if (numeroFatorial == 1) {
-            System.out.println(numeroFatorial);
-        } else {
-            for (int i = numeroFatorial - 2; i > 1; i--) {
-                resultMultiplicacao = resultMultiplicacao * i;
-                if (i == 2) {
-                    System.out.println(resultMultiplicacao);
-                }
-            }
+        int fatorial = 8;
+        long resultado = 1;
+        for (int i = 1; i <= fatorial; i++) {
+            resultado *= i;
         }
+        System.out.println(resultado);
     }
 }
