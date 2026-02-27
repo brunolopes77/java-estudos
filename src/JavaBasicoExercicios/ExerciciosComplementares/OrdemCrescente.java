18620fc8721fd6ea33bec489a47357f0320ea257
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-02-26 23:33:45 -0300

    exercicio

---
+++ src/JavaBasicoExercicios/ExerciciosComplementares/OrdemCrescente.java
@@ -0,0 +1,20 @@
+package JavaBasicoExercicios.ExerciciosComplementares;
+
+public class OrdemCrescente {
+    public static void main(String[] args) {
+        int[] numeros = {4, 7, 3, 8, 2};
+        for (int i = 0; i < numeros.length - 1; i++) {
+            for (int j = i + 1; j < numeros.length ; j++) {
+                if (numeros[i] > numeros[j]) {
+                    int mediadorDeTroca;
+                    mediadorDeTroca = numeros[j];
+                    numeros[j] = numeros[i];
+                    numeros[i] = mediadorDeTroca;;
+                }
+            }
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i]);
+        }
+    }
+}
