6e1f3eeb8a61eb0d8cbb7797e65379a9c695eada
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-03-17 16:53:55 -0300

    começo de set and get

---
+++ src/POO/Introducaometodos/Test/PessoaTest01.java
@@ -0,0 +1,7 @@
+package POO.Introducaometodos.Test;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/POO/Introducaometodos/dominio/Pessoa.java
@@ -0,0 +1,5 @@
+package POO.Introducaometodos.dominio;
+
+public class Pessoa {
+    
+}
