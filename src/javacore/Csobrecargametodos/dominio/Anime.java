683c75414d6ee48aa5de4c00fdede61967014c8d
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-03-23 14:41:52 -0300

    sobrecarga de metodos

--- src/javacore/Csobrecargametodos/dominio/Anime.java
+++ src/javacore/Csobrecargametodos/dominio/Anime.java
@@ -4,11 +4,23 @@ public class Anime {
     private String nome;
     private String tipo;
     private int episodios;
+    private String genero;
 
     public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+    public void init(String nome, String tipo, int episodios){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+    public void init(String nome, String tipo, int episodios, String genero){
+        this.init(nome,tipo, episodios);
+        this.genero = genero;
     }
 
     public void setNome(String nome){
