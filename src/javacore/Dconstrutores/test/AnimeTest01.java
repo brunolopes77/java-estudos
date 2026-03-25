b22f980fbd30834831597c100f4c202d0078b24f
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-03-25 15:20:41 -0300

    construtor

---
+++ src/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package javacore.Dconstrutores.test;
+
+
+import javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Akudama Drive", "TV",12,"Ação");
+        anime.imprime();
+    }
+}
+
+
