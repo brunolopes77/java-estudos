cdc529d635fddfedc8396b00f861aafad748dc8d
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-04-11 20:14:27 -0300

    associacao muitos para um

---
+++ src/javacore/Gassociacao/test/EscolaTest01.java
@@ -0,0 +1,16 @@
+package javacore.Gassociacao.test;
+
+import javacore.Gassociacao.domain.Escola;
+import javacore.Gassociacao.domain.Professor;
+
+public class EscolaTest01 {
+    public static void main(String[] args) {
+        Professor professor1 = new Professor("Bruno");
+        Professor professor2 = new Professor("Vasconcelos");
+
+        Professor[] professores = {professor1,professor2};
+        Escola escola = new Escola("Delta", professores);
+
+        escola.imprime();
+    }
+}
